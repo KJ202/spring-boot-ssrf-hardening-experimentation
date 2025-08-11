6b598aacf77fb3118f8a25d88fdb9046c358f11f
Kian Jamali <kjamali@google.com>
2025-08-11 23:04:36 +0000

    First version of the simple demo

---
+++ smoke-test/spring-boot-apache-demo/src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2012-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	private final RestTemplate restTemplate;
+
+	public DemoApplication(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String response = this.restTemplate.getForObject("https://httpbin.org/get", String.class);
+		System.out.println(response);
+	}
+
+}
